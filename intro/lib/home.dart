662e509b14cffbcda7eebf8d1cec02852fe4f7dd
Srivathsa <bssrivathsa2003@gmail.com>
2022-05-18 08:00:32 +0530

    first commit

---
+++ intro/lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  final int days = 30000;
+  final String name = "srivathsa";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("INTRODUCTION TO FLUTTER"),
+      ),
+    body: Center(
+      child:Container(
+        child: Text("welcome $name your income is $days "),
+      ),
+    ),
+    );
+  }
+}
